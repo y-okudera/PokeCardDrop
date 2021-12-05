df40a353c5355d33ab992c236a35306b35a94ff8
yuoku <appledev.yuoku@gmail.com>
2021-12-05 11:29:16 +0900

    Initial Commit

---
+++ PokeCardDrop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeCardDrop
+//
+//  Created by Yuki Okudera on 2021/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
